1dfd763f19220ff45f35737c76770f19b1d55a69
josemolero18 <joseangelmolero2013@gmail.com>
2025-07-06 23:51:39 -0400

    solucionando postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-import tailwindcss from '@tailwindcss/postcss'
+import tailwindcss, { postcss } from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
 
 export default {
   plugins: [
-    tailwindcss,
+    tailwindcss/postcss,
     autoprefixer
   ]
 }
